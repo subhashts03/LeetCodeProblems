9132c4c6640b861037a4a75b8041e33326c2d351
subhashts03 <149925902+subhashts03@users.noreply.github.com>
2025-01-04 21:20:37 +0530

    Time: 2 ms (69.64%), Space: 44.5 MB (61.71%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int AreaDirectional(int [][] grid,int row,int col){
+        if(row < 0 || row >=grid.length ||col < 0|| col >=grid[0].length || grid[row][col] == 0) 
+            return 0;
+        if(grid[row][col] == -1) 
+            return 0;
+        grid[row][col] = -1;
+        return 1 + AreaDirectional(grid,row + 1,col) //Down
+         + AreaDirectional(grid,row - 1,col) //Up
+         + AreaDirectional(grid,row,col +1) //right
+         + AreaDirectional(grid,row,col - 1);//left
+    }
+    public int maxAreaOfIsland(int[][] grid) {
+        int row = grid.length;
+        int col = grid[0].length;
+        int max = 0;
+        for(int i = 0; i < row; i++){
+            for(int j = 0;j < col; j++){
+                int area = AreaDirectional(grid,i,j);
+                if(max < area){
+                    max = area;
+                }
+            }
+        }
+        return max;
+    }
+}
