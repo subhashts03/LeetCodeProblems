e699206b5e1f883c43e2700fe97894470e1ccf94
subhashts03 <149925902+subhashts03@users.noreply.github.com>
2025-01-21 15:37:40 +0530

    Time: 0 ms (100%), Space: 7.8 MB (56.3%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.c
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.c
@@ -1,16 +1,16 @@
-//Go to Bottomm to Up -> DP;
+//Go to Top to Down;
 int Sum(int n,int dp[]){
     if(n == 0) return 0;
     if(n == 1 || n == 2) return 1;
     
-    dp[0] = 0;
-    dp[1] = 1;
-    dp[2] = 1;
-    for(int i = 3; i < n + 1; i++) 
-        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-    return dp[n];
+    if(dp[n] != -1) return dp[n];
+
+    return dp[n] = Sum(n - 1,dp) + Sum(n - 2,dp) + Sum(n - 3,dp);
 }
 int tribonacci(int n) {
     int dp[n+1];
+    for(int i = 0; i < n + 1; i++){
+        dp[i] = -1;
+    }
     return Sum(n,dp);
 }
