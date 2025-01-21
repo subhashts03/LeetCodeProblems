10c378bbe17d6c2f7b7c703b744c53a0176b3656
subhashts03 <149925902+subhashts03@users.noreply.github.com>
2025-01-21 13:38:07 +0530

    Time: 0 ms (100%), Space: 8 MB (10.01%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.c
+++ 1013-fibonacci-number/1013-fibonacci-number.c
@@ -1,20 +1,14 @@
-//TC : O(2 ^ N)
-// SC : O(N) [Stack] + O(N) [Array] = O(2N) = O(N)
-//DP : Top to Down 
-int Sum(int n ,int arr[]){
-    if(n == 0 || n == 1) return n;
-    if(arr[n] != -1) return arr[n];
-    return arr[n] = Sum(n - 1,arr) + Sum(n - 2,arr);
-}
+//TC : O( N)
+// SC : O(1)
 int fib(int n){
+    int a = 0,b = 1,c = 0;
     if(n == 0 || n == 1) return n;
-   int arr[n+1];
-   for(int i = 0; i < n + 1; i++){
-    arr[i] = -1;
-   } 
-   arr[0] = 0;
-   arr[1] = 1;
-    Sum(n,arr);
-    return arr[n];
+
+    for(int i = 2; i < n + 1; i++){
+        c = a + b;
+        a = b;
+        b = c;
+    }
+    return c;
     
 }
